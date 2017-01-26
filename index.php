c817a83bdcc8053330a72a5c1ec48de2e37e9577
csrnontol <cesar.nontol@gmail.com>
2017-01-26 00:23:10 -0500

    add new images

--- index.php
+++ index.php
@@ -5,3 +5,17 @@
  * Date: 25/01/2017
  * Time: 12:49 PM
  */
+?>
+<!doctype html>
+<html lang="es-PE">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+//
+</body>
+</html>
