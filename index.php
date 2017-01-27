09140977b3bb17b32e92f77edfc6a7be1dda0721
csrnontol <cesar.nontol@gmail.com>
2017-01-27 01:46:02 -0500

    avance pagina de inicio

--- index.php
+++ index.php
@@ -14,8 +14,52 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/master.css">
 </head>
 <body>
-//
+<header class="main-header">
+    <div class="main-logo">
+        <div>
+            <a href="">
+                <img src="img/logo.png" alt="Logo de la Organización">
+                <h3 class="slogan">Jubilados a Luchar</h3>
+            </a>
+        </div>
+    </div>
+    <nav class="main-menu">
+        <div class="clearfix">
+            <div>
+                <a href="">
+                    <h4>¿Quiénes somos?</h4>
+                    <p>Situación de los jubilados</p>
+                </a>
+            </div>
+            <div>
+                <a href="">
+                    <h4>Aportes y opiniones</h4>
+                    <p>La jubilación en el país</p>
+                </a>
+            </div>
+            <div>
+                <a href="">
+                    <h4>Contáctenos</h4>
+                    <p>Ubícación y contacto</p>
+                </a>
+            </div>
+        </div>
+    </nav>
+</header>
+<main>
+    <section>
+        // Organizacion, Misión, Visión
+    </section>
+    <aside>
+        // Sesión del usuario
+    </aside>
+</main>
+<footer>
+    // footer
+</footer>
 </body>
 </html>
