381627a23eb48e4c8865d4377e7e47b69c96774c
csrnontol <cesar.nontol@gmail.com>
2017-02-26 13:15:45 -0500

    avance contacto

--- contacto.php
+++ contacto.php
@@ -6,6 +6,64 @@
  * Time: 23:46
  */
 session_start();
+$contact_errors = '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $contact_name = trim($_POST['in-name']);
+    $contact_email = trim($_POST['in-email']);
+    $contact_message = trim($_POST['txt-message']);
+
+    /* regular expressions */
+    $namesRx = "/^[a-zA-ZÀ-ú ]{3,100}$/";
+    $errors = 0;
+
+    if (empty($contact_name)) {
+        $errors++;
+        $contact_errors = '<span>- Nombre es requerido.</span>';
+    } elseif (!preg_match($namesRx, $contact_name)) {
+        $errors++;
+        $contact_errors = '<span>- Sólo letras y espacios en un mínimo de tres.</span>';
+    }
+    if (empty($contact_email)) {
+        $errors++;
+        $contact_errors .= '<span>- E-mail es requerido para enviar una respuesta.</span>';
+    } elseif (!filter_var($contact_email, FILTER_VALIDATE_EMAIL)) {
+        $errors++;
+        $contact_errors .= '<span>- El formato del e-amil ingresado no es válido.</span>';
+    }
+    if (empty($contact_message)) {
+        $errors++;
+        $contact_errors .= '<span>- Mensaje es requerido.</span>';
+    } elseif (strlen($contact_message) < 10) {
+        $errors++;
+        $contact_errors .= '<span>- Por favor especifique su mensaje.</span>';
+    }
+
+    if ($errors == 0) {
+        $contact_message = preg_replace('/\r\n|\r|\n/', '<br>', $contact_message);
+        $contact_message = wordwrap($contact_message, 70, "\r\n");
+        /*vars for confirmation email*/
+        $mailSubject = "Jubilados a Luchar - Mensaje de Contacto";
+        $mailSubject = "=?UTF-8?B?" . base64_encode($mailSubject) . "?=";
+        $mailHeadings = array();
+        $mailHeadings[] = "MIME-Version: 1.0";
+        $mailHeadings[] = "Content-type: text/html; charset=ISO-8859-1";
+        $mailHeadings[] = "Content-Type: text/html; charset=UTF-8";
+        $mailHeadings[] = "From: Asociación Jubilados a Luchar";
+        $mailBody = "Se ha recibido un nuevo mensaje de la página de contacto de Jubilados a Luchar:<br><br>";
+        $mailBody .= "<b>Nombres:</b> $contact_name<br>";
+        $mailBody .= "<b>E-mail:</b> $contact_email<br>";
+        $mailBody .= "<b>Mensaje:</b> $contact_message<br>";
+        $mailBody .= "<br><br><br>Atte.:<br>Sistema Web (jubiladosaluchar.com)";
+        /*send confirmation mail*/
+        if (mail('cormas14@hotmail.com', $mailSubject, $mailBody, implode("\r\n", $mailHeadings))) {
+            header('Location: contacto-result.php?result=success');
+            exit();
+        } else {
+            header('Location: contacto-result.php?result=notsent');
+            exit();
+        }
+    }
+}
 ?>
 <!doctype html>
 <html lang="es-PE">
@@ -27,28 +85,29 @@ include_once 'tools/menu-and-user-login.php';
 ?>
 <main class="_main-container">
     <section class="content-mainsection contacto-form">
-        <div class="contacto-ubicacion">
+        <div class="contacto-ubicacion clear-content-div">
             <div id="contactanos">
-                <form id="contact_form" action="#" method="post">
+                <form id="contact_form" action="" method="post">
+                    <h2 class="contact-title">Formulario de contacto</h2>
                     <div class="row">
-                        <label for="name">Nombre:&nbsp;&nbsp;</label>
-                        <img src="img/default-avatar.png">
-                        <br/>
-                        <input id="name" class="input" name="name" type="text" value="" /><br />
+                        <label for="contact-name"><i class="fa fa-user"></i> Nombres: <span>(Requerido)</span></label>
+                        <input id="contact-name" class="input" name="in-name" type="text" value="" />
                     </div>
                     <div class="row">
-                        <label for="email">Email:&nbsp;&nbsp;</label>
-                        <img src="img/email.png">
-                        <br/>
-                        <input id="email" class="input" name="email" type="text" value=""  /><br />
+                        <label for="contact-email"><i class="fa fa-envelope"></i> E-mail: <span>(Requerido)</span></label>
+                        <input id="contact-email" class="input" name="in-email" type="email" value="" />
                     </div>
                     <div class="row">
-                        <label for="message">Mensaje:&nbsp;&nbsp;</label>
-                        <img src="img/contact.png">
-                        <textarea id="message" class="input" name="message" rows="7" cols="30"></textarea><br />
+                        <label for="contact-message"><i class="fa fa-pencil"></i> Mensaje: <span>(Requerido)</span></label>
+                        <textarea id="contact-message" class="input" name="txt-message" rows="7" cols="30"></textarea>
                     </div>
-                    </br>
-                    <input id="submit_button" type="submit" value="Enviar" />
+                    <div class="div-button row">
+                        <div class="contact-errors"><?= $contact_errors; ?></div>
+                        <div class="loading-request--gif" style="display: none;"></div>
+                        <button type="submit" class="_btnDisabled" name="btn-send-contact" id="btn-send-contact">
+                            <i class="fa fa-paper-plane"></i> Enviar mensaje
+                        </button>
+                    </div>
                 </form>
             </div>
         </div>
--- js/global-functions.js
+++ js/global-functions.js
@@ -104,6 +104,32 @@ $(document).ready(function () {
         $(divMainMenu).toggleClass("active");
         $(divSessionInside).toggleClass("active");
     });
+
+
+    /*
+     * contact form
+     */
+    var contactForm = document.getElementById("contact_form");
+    var btnSendContact = document.getElementById("btn-send-contact");
+    var contactErrors = document.getElementsByClassName("contact-errors")[0];
+    $(contactForm).on('submit', function () {
+        if ($(btnSendContact).hasClass("_btnDisabled")) {
+            return false;
+        }
+        var processingGif = document.getElementsByClassName("loading-request--gif")[0];
+        $(processingGif).fadeIn(100);
+        return validateContactForm();
+    });
+    var contactFormElemenets = contactForm.elements;
+    $(contactFormElemenets).on('input', function () {
+        if (this.value.trim().length > 0) {
+            $(btnSendContact).removeClass("_btnDisabled");
+        } else {
+            $(btnSendContact).addClass("_btnDisabled");
+        }
+        $(this).css('border-color', '');
+        contactErrors.innerHTML = '';
+    });
 });
 
 
@@ -141,4 +167,56 @@ function verifyUserLogin(user, pass, callback) {
             callback(); // see $.when, promise()
         }
     });
+}
+
+
+/* function to validate contact form */
+function validateContactForm() {
+    var inNames = document.getElementById("contact-name");
+    var inEmail = document.getElementById("contact-email");
+    var inMessage = document.getElementById("contact-message");
+
+    var namesValue = inNames.value.trim();
+    var emailValue = inEmail.value.trim();
+    var messageValue = inMessage.value.trim();
+
+    var contactErrors = document.getElementsByClassName("contact-errors")[0];
+    var errors = 0;
+    var errorColor = '#b05752';
+    var rxEmail = /^[A-Za-z][A-Za-z0-9._-]*@[A-Za-z0-9_-]+\.[A-Za-z0-9_.]+[A-za-z]$/;
+    var rxUsername = /^[a-zA-ZÀ-ú ]{3,100}$/;
+    var processingGif = document.getElementsByClassName("loading-request--gif")[0];
+
+    contactErrors.innerHTML = '';
+    $(inNames).add($(inEmail)).add($(inMessage)).css('border-color', '');
+
+    if (namesValue == '' || !rxUsername.test(namesValue)) {
+        errors++;
+        inNames.style.borderColor = errorColor;
+        if (errors == 1) inNames.focus();
+        if (!rxUsername.test(namesValue)) {
+            contactErrors.innerHTML += '<span>- Ingresar sólo letras y espacios (mín. 3, máx. 100).</span>';
+        }
+    }
+
+    if (emailValue == '' || !rxEmail.test(emailValue)) {
+        errors++;
+        inEmail.style.borderColor = errorColor;
+        if (errors == 1) inEmail.focus();
+        if (!rxEmail.test(emailValue)) {
+            contactErrors.innerHTML += '<span>- El formato del e-amil ingresado no es válido.</span>';
+        }
+    }
+
+    if (messageValue == '' || messageValue.length < 10) {
+        errors++;
+        inMessage.style.borderColor = errorColor;
+        if (errors == 1) inMessage.focus();
+        if (messageValue.length < 10) {
+            contactErrors.innerHTML += '<span>- Por favor especifique su mensaje.</span>';
+        }
+    }
+
+    $(processingGif).fadeOut();
+    return errors <= 0;
 }
